74c857d69c9cf65e6359881575eca06368d04849
Matias Damian H <matiasdhuyema@gmail.com>
2026-06-11 20:08:57 -0300

    commit2

--- src/app/components/motion/motion.ts
+++ src/app/components/motion/motion.ts
@@ -27,6 +27,15 @@ export class Motion implements OnInit {
   }
 
   misMotions: Array<Video> = [
+       {
+      titulo: 'Publicity Ads',
+      categoria: 'Motion Graphics Ads · After Effects',
+      colorBadge: 'text-primary',
+      imagenPortada: '',
+      linkVideo: 'https://youtube.com/shorts/eWcYkQgDo-Y',
+      claseBento: 'col-span-2',
+      linkExterno: ''
+    },
     {
       titulo: 'Full Album Visualizer',
       categoria: 'Omar Varela x Ecko',
@@ -78,15 +87,6 @@ export class Motion implements OnInit {
       colorBadge: 'text-primary',
       imagenPortada: '',
       linkVideo: 'https://youtu.be/Hh8Bz5BmQ0s',
-      claseBento: 'col-span-2',
-      linkExterno: ''
-    },
-    {
-      titulo: 'Publicity Ads',
-      categoria: 'Motion Graphics Ads · After Effects',
-      colorBadge: 'text-primary',
-      imagenPortada: '',
-      linkVideo: 'https://youtube.com/shorts/eWcYkQgDo-Y',
       claseBento: 'col-span-2',
       linkExterno: ''
     }
